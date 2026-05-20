e2d8cc14347b31edaf3198bc397c3eee02987bb1
vandogouveia <vando_gouveia@hotmail.com>
2026-05-20 10:09:58 -0300

    ajuste arquivos polo

--- controllers/consorcio/arquivosPoloController.js
+++ controllers/consorcio/arquivosPoloController.js
@@ -64,7 +64,7 @@ module.exports = {
     },
 
     getAllArquivos(req, res) {
-        const selectArquivo = `SELECT * FROM arquivos_polo ORDER BY date DESC LIMIT 4`;
+        const selectArquivo = `SELECT * FROM arquivos_polo ORDER BY date DESC`;
 
         connection.query(selectArquivo, [], function (error, results, fields) {
             if (error) {
